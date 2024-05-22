106b6ee40496d0b2e6a5ab747d7193e036a612f1
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2024-05-22 17:55:59 +0800

    update: finalize repo

---
+++ README.md
@@ -0,0 +1,12 @@
+# Introduction
+This is a vision application that allows you to control some popular game via body movement by utilizing real-time pose detection.
+
+# List of games
+You can play any game which controlled by keyboard with this application. Some examples:
+
+- [Chrome Dino](chromedino.com)
+- [Subway Surfers](https://poki.com/en/g/subway-surfers)
+
+# How to use
+1. ```pip install -r requirements.txt```
+2. ```python myGame.py```
--- myPose.py
+++ myPose.py
@@ -1,29 +1,32 @@
-import mediapipe as mp
-import cv2
 from math import hypot
 
+import cv2
+import mediapipe as mp
+from matplotlib import pyplot as plt
 
-class myPose():
+
+class myPose:
     def __init__(self):
-        # self.mp_pose = mp.solutions.pose
-        # self.pose = self.mp_pose.Pose()
-        # self.mp_drawing = mp.solutions.drawing_utils
-        # self.shoudler_line_y = 0  # Luu lai cai vi tri 2 vai cua nguoi dung khi vo tay bat dau game
-
         # Initialize mediapipe pose class.
         self.mp_pose = mp.solutions.pose
 
         # Setup the Pose function for images.
-        self.pose_image = self.mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5, model_complexity=1)
+        self.pose_image = self.mp_pose.Pose(
+            static_image_mode=True, min_detection_confidence=0.5, model_complexity=1
+        )
 
         # Setup the Pose function for videos.
-        self.pose_video = self.mp_pose.Pose(static_image_mode=False, model_complexity=1, min_detection_confidence=0.7,
-                                            min_tracking_confidence=0.7)
+        self.pose_video = self.mp_pose.Pose(
+            static_image_mode=False,
+            model_complexity=1,
+            min_detection_confidence=0.7,
+            min_tracking_confidence=0.7,
+        )
 
         # Initialize mediapipe drawing class.
         self.mp_drawing = mp.solutions.drawing_utils
 
-    def detectPose(self, image, pose, draw=False, display=False):
+    def detect_pose(self, image, pose, draw=False, display=False):
         """
         This function performs the pose detection on the most prominent person in an image.
         Args:
@@ -51,32 +54,37 @@ class myPose():
         # Check if any landmarks are detected and are specified to be drawn.
         if results.pose_landmarks and draw:
             # Draw Pose Landmarks on the output image.
-            self.mp_drawing.draw_landmarks(image=output_image, landmark_list=results.pose_landmarks,
-                                      connections=self.mp_pose.POSE_CONNECTIONS,
-                                      landmark_drawing_spec=self.mp_drawing.DrawingSpec(color=(255, 255, 255),
-                                                                                   thickness=3, circle_radius=3),
-                                      connection_drawing_spec=self.mp_drawing.DrawingSpec(color=(49, 125, 237),
-                                                                                     thickness=2, circle_radius=2))
+            self.mp_drawing.draw_landmarks(
+                image=output_image,
+                landmark_list=results.pose_landmarks,
+                connections=self.mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=self.mp_drawing.DrawingSpec(
+                    color=(255, 255, 255), thickness=3, circle_radius=3
+                ),
+                connection_drawing_spec=self.mp_drawing.DrawingSpec(
+                    color=(49, 125, 237), thickness=2, circle_radius=2
+                ),
+            )
 
         # Check if the original input image and the resultant image are specified to be displayed.
         if display:
             # Display the original input image and the resultant image.
             plt.figure(figsize=[22, 22])
-            plt.subplot(121);
-            plt.imshow(image[:, :, ::-1]);
-            plt.title("Original Image");
-            plt.axis('off');
-            plt.subplot(122);
-            plt.imshow(output_image[:, :, ::-1]);
-            plt.title("Output Image");
-            plt.axis('off');
+            plt.subplot(121)
+            plt.imshow(image[:, :, ::-1])
+            plt.title("Original Image")
+            plt.axis("off")
+            plt.subplot(122)
+            plt.imshow(output_image[:, :, ::-1])
+            plt.title("Output Image")
+            plt.axis("off")
 
         # Otherwise
         else:
             # Return the output image and the results of pose landmarks detection.
             return output_image, results
 
-    def checkPose_LRC(self, image, results, draw=False, display=False):
+    def check_pose_LRC(self, image, results, draw=False, display=False):
         """
         This function finds the horizontal position (left, center, right) of the person in an image.
         Args:
@@ -100,57 +108,67 @@ class myPose():
         output_image = image.copy()
 
         # Retreive the x-coordinate of the left shoulder landmark.
-        left_x = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].x * width)
+        left_x = int(
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].x
+            * width
+        )
 
         # Retreive the x-corrdinate of the right shoulder landmark.
-        right_x = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].x * width)
+        right_x = int(
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].x
+            * width
+        )
 
         # Check if the person is at left that is when both shoulder landmarks x-corrdinates
         # are less than or equal to the x-corrdinate of the center of the image.
-        if (right_x <= width // 2 and left_x <= width // 2):
-
+        if right_x <= width // 2 and left_x <= width // 2:
             # Set the person's position to left.
-            horizontal_position = 'Left'
+            horizontal_position = "Left"
 
         # Check if the person is at right that is when both shoulder landmarks x-corrdinates
         # are greater than or equal to the x-corrdinate of the center of the image.
-        elif (right_x >= width // 2 and left_x >= width // 2):
-
+        elif right_x >= width // 2 and left_x >= width // 2:
             # Set the person's position to right.
-            horizontal_position = 'Right'
+            horizontal_position = "Right"
 
         # Check if the person is at center that is when right shoulder landmark x-corrdinate is greater than or equal to
         # and left shoulder landmark x-corrdinate is less than or equal to the x-corrdinate of the center of the image.
-        elif (right_x >= width // 2 and left_x <= width // 2):
-
+        elif right_x >= width // 2 and left_x <= width // 2:
             # Set the person's position to center.
-            horizontal_position = 'Center'
+            horizontal_position = "Center"
 
         # Check if the person's horizontal position and a line at the center of the image is specified to be drawn.
         if draw:
             # Write the horizontal position of the person on the image.
-            cv2.putText(output_image, horizontal_position, (5, height - 10), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255),
-                        3)
+            cv2.putText(
+                output_image,
+                horizontal_position,
+                (5, height - 10),
+                cv2.FONT_HERSHEY_PLAIN,
+                2,
+                (255, 255, 255),
+                3,
+            )
 
             # Draw a line at the center of the image.
-            cv2.line(output_image, (width // 2, 0), (width // 2, height), (255, 255, 255), 2)
+            cv2.line(
+                output_image, (width // 2, 0), (width // 2, height), (255, 255, 255), 2
+            )
 
         # Check if the output image is specified to be displayed.
         if display:
-
             # Display the output image.
             plt.figure(figsize=[10, 10])
-            plt.imshow(output_image[:, :, ::-1]);
-            plt.title("Output Image");
-            plt.axis('off');
+            plt.imshow(output_image[:, :, ::-1])
+            plt.title("Output Image")
+            plt.axis("off")
 
         # Otherwise
         else:
-
             # Return the output image and the person's horizontal position.
             return output_image, horizontal_position
 
-    def checkPose_JSD(self, image, results, MID_Y=250, draw=False, display=False):
+    def check_pose_JSD(self, image, results, MID_Y=250, draw=False, display=False):
         """
         This function checks the posture (Jumping, Crouching or Standing) of the person in an image.
         Args:
@@ -172,63 +190,72 @@ class myPose():
         output_image = image.copy()
 
         # Retreive the y-coordinate of the left shoulder landmark.
-        left_y = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].y * height)
+        left_y = int(
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_SHOULDER].y
+            * height
+        )
 
         # Retreive the y-coordinate of the right shoulder landmark.
-        right_y = int(results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].y * height)
+        right_y = int(
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_SHOULDER].y
+            * height
+        )
 
         # Calculate the y-coordinate of the mid-point of both shoulders.
         actual_mid_y = abs(right_y + left_y) // 2
 
         # Calculate the upper and lower bounds of the threshold.
-        lower_bound = MID_Y - 15
-        upper_bound = MID_Y + 100
+        lower_bound = MID_Y - 35
+        upper_bound = MID_Y + 35
 
         # Check if the person has jumped that is when the y-coordinate of the mid-point
         # of both shoulders is less than the lower bound.
-        if (actual_mid_y < lower_bound):
-
+        if actual_mid_y < lower_bound:
             # Set the posture to jumping.
-            posture = 'Jumping'
+            posture = "Jumping"
 
         # Check if the person has crouched that is when the y-coordinate of the mid-point
         # of both shoulders is greater than the upper bound.
-        elif (actual_mid_y > upper_bound):
-
+        elif actual_mid_y > upper_bound:
             # Set the posture to crouching.
-            posture = 'Crouching'
+            posture = "Crouching"
 
         # Otherwise the person is standing and the y-coordinate of the mid-point
         # of both shoulders is between the upper and lower bounds.
         else:
-
             # Set the posture to Standing straight.
-            posture = 'Standing'
+            posture = "Standing"
 
         # Check if the posture and a horizontal line at the threshold is specified to be drawn.
         if draw:
             # Write the posture of the person on the image.
-            cv2.putText(output_image, posture, (5, height - 50), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 3)
+            cv2.putText(
+                output_image,
+                posture,
+                (5, height - 50),
+                cv2.FONT_HERSHEY_PLAIN,
+                2,
+                (255, 255, 255),
+                3,
+            )
 
             # Draw a line at the intial center y-coordinate of the person (threshold).
             cv2.line(output_image, (0, MID_Y), (width, MID_Y), (255, 255, 255), 2)
 
         # Check if the output image is specified to be displayed.
         if display:
-
             # Display the output image.
             plt.figure(figsize=[10, 10])
-            plt.imshow(output_image[:, :, ::-1]);
-            plt.title("Output Image");
-            plt.axis('off');
+            plt.imshow(output_image[:, :, ::-1])
+            plt.title("Output Image")
+            plt.axis("off")
 
         # Otherwise
         else:
-
             # Return the output image and posture indicating whether the person is standing straight or has jumped, or crouched.
             return output_image, posture
 
-    def checkHandsJoined(self, image, results, draw=False, display=False):
+    def check_hands_joined(self, image, results, draw=False, display=False):
         """
         This function checks whether the hands of the person are joined or not in an image.
         Args:
@@ -250,31 +277,41 @@ class myPose():
         output_image = image.copy()
 
         # Get the left wrist landmark x and y coordinates.
-        left_wrist_landmark = (results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].x * width,
-                               results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].y * height)
+        left_wrist_landmark = (
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].x
+            * width,
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.LEFT_WRIST].y
+            * height,
+        )
 
         # Get the right wrist landmark x and y coordinates.
-        right_wrist_landmark = (results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].x * width,
-                                results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].y * height)
+        right_wrist_landmark = (
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].x
+            * width,
+            results.pose_landmarks.landmark[self.mp_pose.PoseLandmark.RIGHT_WRIST].y
+            * height,
+        )
 
         # Calculate the euclidean distance between the left and right wrist.
-        euclidean_distance = int(hypot(left_wrist_landmark[0] - right_wrist_landmark[0],
-                                       left_wrist_landmark[1] - right_wrist_landmark[1]))
+        euclidean_distance = int(
+            hypot(
+                left_wrist_landmark[0] - right_wrist_landmark[0],
+                left_wrist_landmark[1] - right_wrist_landmark[1],
+            )
+        )
 
         # Compare the distance between the wrists with a appropriate threshold to check if both hands are joined.
-        if euclidean_distance < 100:
-
+        if euclidean_distance < 130:
             # Set the hands status to joined.
-            hand_status = 'Hands Joined'
+            hand_status = "Hands Joined"
 
             # Set the color value to green.
             color = (0, 255, 0)
 
         # Otherwise.
         else:
-
             # Set the hands status to not joined.
-            hand_status = 'Hands Not Joined'
+            hand_status = "Hands Not Joined"
 
             # Set the color value to red.
             color = (0, 0, 255)
@@ -282,24 +319,30 @@ class myPose():
         # Check if the Hands Joined status and hands distance are specified to be written on the output image.
         if draw:
             # Write the classified hands status on the image.
-            cv2.putText(output_image, hand_status, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+            cv2.putText(
+                output_image, hand_status, (10, 30), cv2.FONT_HERSHEY_PLAIN, 2, color, 3
+            )
 
             # Write the the distance between the wrists on the image.
-            cv2.putText(output_image, f'Distance: {euclidean_distance}', (10, 70),
-                        cv2.FONT_HERSHEY_PLAIN, 2, color, 3)
+            cv2.putText(
+                output_image,
+                f"Distance: {euclidean_distance}",
+                (10, 70),
+                cv2.FONT_HERSHEY_PLAIN,
+                2,
+                color,
+                3,
+            )
 
         # Check if the output image is specified to be displayed.
         if display:
-
             # Display the output image.
             plt.figure(figsize=[10, 10])
-            plt.imshow(output_image[:, :, ::-1]);
-            plt.title("Output Image");
-            plt.axis('off');
+            plt.imshow(output_image[:, :, ::-1])
+            plt.title("Output Image")
+            plt.axis("off")
 
         # Otherwise
         else:
-
             # Return the output image and the classified hands status indicating whether the hands are joined or not.
             return output_image, hand_status
-
